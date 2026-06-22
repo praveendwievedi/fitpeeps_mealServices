1e330105dbd29ead253f9f381ed0f56ca7b1de50
Praveen Kumar <praveen.kumar22@globallogic.com>
2026-06-22 14:40:18 +0530

    initial commit

---
+++ src/test/java/meal_service/MealServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package meal_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
